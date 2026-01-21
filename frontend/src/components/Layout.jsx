22f78f46b813da069b2c1d9a44d485c0c32a82aa
alpaqers <pawko0202@gmail.com>
2026-01-21 12:34:14 +0100

    style: layout page adjustments

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,7 +6,7 @@ export function Layout({ children }) {
       <div className="flex min-h-screen flex-col">
         <Header />
   
-        <main className="mx-auto w-screen py-8">
+        <main className="w-full">
           {children}
         </main>
         <Footer />
